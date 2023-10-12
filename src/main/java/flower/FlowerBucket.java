4c6259591bc2ea17fabd514556036aae9bb2a6d1
Karl9Doniz <andrei.agitolyev@gmail.com>
2023-10-12 12:35:16 +0300

    added store package, flower bucket and flower pack functionality

---
+++ src/main/java/flower/FlowerBucket.java
@@ -0,0 +1,24 @@
+package flower;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter @NoArgsConstructor
+public class FlowerBucket {
+    private final List<FlowerPack> flowerPacks = new ArrayList<FlowerPack>();
+
+    public void addFlowerPack(FlowerPack flowerPack) {
+        flowerPacks.add(flowerPack);
+    }
+
+    public double getPrice() {
+        double price = 0;
+        for (FlowerPack flowerPack: flowerPacks) {
+            price += flowerPack.getPrice();
+        }
+        return price;
+    }
+}
---
+++ src/main/java/store/Filter.java
@@ -0,0 +1,5 @@
+package store;
+
+public abstract class Filter {
+    abstract boolean match(Item item);
+}
